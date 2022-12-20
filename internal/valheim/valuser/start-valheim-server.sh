d8edc17b5f16088714dada4791de03e721d461df
Alex Nordhausen <anorthhouse@gmail.com>
2022-12-20 14:13:03 -0700

    chore: valheim and barotrauma

--- internal/valheim/valuser/start-valheim-server.sh
+++ internal/valheim/valuser/start-valheim-server.sh
@@ -4,5 +4,5 @@ export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppID=892970
 
 echo "Starting server PRESS CTRL-C to exit"  
-./valheim_server.x86_64 -name "NamonesDedicatedValheimServer" -port 2456 -nographics -batchmode -world "Ragnarok" -password "muggins1" -savedir "/root/.config/unity3d/IronGate/Valheim" -public 1  
+./valheim_server.x86_64 -name "NamonesDedicatedValheimServer" -port 2456 -nographics -batchmode -world "DedicatedWorld" -password "muggins1" -savedir "/root/.config/unity3d/IronGate/Valheim" -public 1  
 export LD_LIBRARY_PATH=$templdpath
